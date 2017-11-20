90ec932b3d335348e8ef5bb480e17105c8cb279a
Paulo <pauloilarde@gmail.com>
2017-11-20 20:13:51 -0200

    Melhorando loops while e for

---
+++ RoadTrip2/Level1/scripts/trainFor.js
@@ -0,0 +1,8 @@
+var trainNumber = 1;
+var trainsOperational = 8;
+var totalTrains = 12;
+var stoppedTrain = trainsOperational + 1;
+
+for(stoppedTrain; stoppedTrain <= totalTrains; stoppedTrain++){
+   console.log('Train #' + stoppedTrain + ' is not operational.');
+}
---
+++ RoadTrip2/Level1/scripts/trainWhile.js
@@ -0,0 +1,9 @@
+var trainNumber = 1;
+var trainsOperational = 9;
+var totalTrains = 12;
+
+console.log('There are ' + trainsOperational + ' trains running today.');
+while (trainNumber <= trainsOperational ) {
+   console.log('Train #' + trainNumber + ' is running.');
+   trainNumber++;
+}
