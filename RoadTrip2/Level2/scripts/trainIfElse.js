acbcd4c086530b73b87cc653de94b52c0c0f5f0d
Paulo <pauloilarde@gmail.com>
2017-11-20 20:50:45 -0200

    Nested conditionals

--- RoadTrip2/Level2/scripts/trainIfElse.js
+++ RoadTrip2/Level2/scripts/trainIfElse.js
@@ -1,14 +1,24 @@
 var trainNumber = 1;
-var trainsOperational = 8;
+var trainsOperational = 5;
 var totalTrains = 12;
 var stoppedTrain = trainsOperational + 1;
 
-for(trainNumber; trainNumber <= totalTrains; trainNumber++){
-   if(trainNumber <= trainsOperational){
-      console.log('Train #' + trainNumber + ' is running.');
-   }else if(trainNumber == 10){
-      console.log('Train #10 begins to run at noon.');
-   }else {
-      console.log('Train #' + trainNumber + ' is not operational.');
+
+if (trainsOperational > 0) {
+   if( trainsOperational == totalTrains){
+      console.log('All trains are running at JavaScript express.');
+   }else{
+      for(trainNumber; trainNumber <= totalTrains; trainNumber++){
+         if(trainNumber <= trainsOperational){
+            console.log('Train #' + trainNumber + ' is running.');
+         }else if(trainNumber == 10){
+            console.log('Train #10 begins to run at noon.');
+         }else {
+            console.log('Train #' + trainNumber + ' is not operational.');
+         }
+      }
    }
+
+} else {
+   console.log('No trains are operational today! :/');
 }
