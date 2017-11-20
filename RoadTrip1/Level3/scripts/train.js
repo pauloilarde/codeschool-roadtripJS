46525ae1bbd4ec9bc184eef7827261ecddcdaf4c
Paulo <pauloilarde@gmail.com>
2017-11-20 19:45:42 -0200

    Conhecendo variáveis e o console.log

---
+++ RoadTrip1/Level3/scripts/train.js
@@ -0,0 +1,16 @@
+
+
+
+
+var totalTrains = 12;
+var trainsOperational = 8;
+
+console.log('There are ' + trainsOperational + ' trains running today.');
+console.log('Train #' + 1 + ' is running.');
+console.log('Train #' + 2 + ' is running.');
+console.log('Train #' + 3 + ' is running.');
+console.log('Train #' + 4 + ' is running.');
+console.log('Train #' + 5 + ' is running.');
+console.log('Train #' + 6 + ' is running.');
+console.log('Train #' + 3 + ' is running.');
+console.log('Train #' + 8 + ' is running.');
