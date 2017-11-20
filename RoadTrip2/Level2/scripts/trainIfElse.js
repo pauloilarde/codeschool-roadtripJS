10006121ff38c344b5e7be4396434a2f6d0d6134
Paulo <pauloilarde@gmail.com>
2017-11-20 20:41:23 -0200

    Sintaxe else-if

--- RoadTrip2/Level2/scripts/trainIfElse.js
+++ RoadTrip2/Level2/scripts/trainIfElse.js
@@ -6,8 +6,9 @@ var stoppedTrain = trainsOperational + 1;
 for(trainNumber; trainNumber <= totalTrains; trainNumber++){
    if(trainNumber <= trainsOperational){
       console.log('Train #' + trainNumber + ' is running.');
+   }else if(trainNumber == 10){
+      console.log('Train #10 begins to run at noon.');
    }else {
       console.log('Train #' + trainNumber + ' is not operational.');
    }
-
 }
